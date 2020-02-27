32d3169bfbe6d6a6607b36f4570d472951eb41ca
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-02-27 16:46:51 +0100

    Render one city details on the main page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import { Button } from 'reactstrap';
 import * as data from 'german-license-plate-prefixes';
 
-import logo from './logo.svg';
 import './App.css';
 
-console.log(data.default);
+const codeName = 'A';
+const cityName = data.default[codeName];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button color="success">Success</Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+          <tbody>
+            <tr>
+              <td>{codeName}</td>
+              <td>{cityName}</td>
+            </tr>
+          </tbody>
+        </table>
       </header>
     </div>
   );
