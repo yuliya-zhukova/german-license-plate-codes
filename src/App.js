9abdd8e06c419dc4a0188548d245395dc66b1fff
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-03-11 17:17:45 +0100

    Remove header

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import {data} from './mock-data';
 function App() {
   return (
     <div className="App">
-      <table>
-        <thead>
-          <tr>
-            <th class="code-column">City code</th>
-            <th>City name</th>
-          </tr>
-        </thead>
-      </table>
       {RenderRow(data[0])}
       {RenderRow(data[1])}
       {RenderRow(data[2])}
