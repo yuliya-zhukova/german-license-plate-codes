50a647b72ee618c259ead78de8bb11625d7ece59
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-03-11 10:42:36 +0100

    Remove redundant code and files

--- src/App.js
+++ src/App.js
@@ -3,8 +3,40 @@ import * as data from 'german-license-plate-prefixes';
 
 import './App.css';
 
+const cleanData = data.default;
+
+
 const codeName = 'A';
-const cityName = data.default[codeName];
+const cityName = cleanData[codeName];
+
+const codeName2 = 'FFB';
+const cityName2 = cleanData[codeName2];
+
+function renderTableRow(codeName, cityName) {
+  return (
+    <tr>
+      <td>{codeName}</td>
+      <td>{cityName}</td>
+    </tr>
+  );
+}
+
+function transformToArray(data) {
+  const allRows = [];
+
+  for (let [key, value] of Object.entries(data)) {
+    allRows.push({
+      key: key,
+      value: value
+    });
+  } 
+  
+  return allRows;
+}
+
+const arrayData = transformToArray(data)
+
+console.log(arrayData);
 
 function App() {
   return (
@@ -12,10 +44,8 @@ function App() {
       <header className="App-header">
         <table>
           <tbody>
-            <tr>
-              <td>{codeName}</td>
-              <td>{cityName}</td>
-            </tr>
+            {renderTableRow(codeName, cityName)}
+            {renderTableRow(codeName2, cityName2)}
           </tbody>
         </table>
       </header>
