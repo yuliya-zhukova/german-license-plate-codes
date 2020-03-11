cd6e1c402569779dcaa21d10f48b1e960af777ba
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-03-11 11:11:04 +0100

    Create new component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,12 @@
 import React from 'react';
-import * as data from 'german-license-plate-prefixes';
 
 import './App.css';
-
-const cleanData = data.default;
-
-
-const codeName = 'A';
-const cityName = cleanData[codeName];
-
-const codeName2 = 'FFB';
-const cityName2 = cleanData[codeName2];
-
-function renderTableRow(codeName, cityName) {
-  return (
-    <tr>
-      <td>{codeName}</td>
-      <td>{cityName}</td>
-    </tr>
-  );
-}
-
-function transformToArray(data) {
-  const allRows = [];
-
-  for (let [key, value] of Object.entries(data)) {
-    allRows.push({
-      key: key,
-      value: value
-    });
-  } 
-  
-  return allRows;
-}
-
-const arrayData = transformToArray(data)
-
-console.log(arrayData);
+import { RenderRow } from './RenderRow';
 
 function App() {
   return (
     <div className="App">
-        <table>
-          <tbody>
-            {renderTableRow(codeName, cityName)}
-            {renderTableRow(codeName2, cityName2)}
-          </tbody>
-        </table>
+      {RenderRow()}
     </div>
   );
 }
---
+++ src/RenderRow.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function RenderRow() {
+    return (
+      <div>
+        Hallo!
+      </div>
+    );
+}
