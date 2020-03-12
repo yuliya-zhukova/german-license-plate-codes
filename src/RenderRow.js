95c6964d284adcbe8538d7580c6d7082e73c6bc2
Yuliya Zhukova <zhkv.jl@gmail.com>
2020-03-12 18:03:28 +0100

    Fix render row

--- src/RenderRow.js
+++ src/RenderRow.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 export function RenderRow(props) {
     return (
-      <div>
         <tr>
           <td>{props.cityCode}</td>
           <td>{props.cityName}</td>
         </tr>
-      </div>
     );
 }
