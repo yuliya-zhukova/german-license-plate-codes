2ef2cf27fa6dc6c9c49ae3d7d789d77f589c528d
Yuliya Zhukova <zhkv.jl@gmail.com>
2021-03-16 21:20:49 +0100

    Add SearchField component

--- src/SearchField.tsx
+++ src/SearchField.tsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+
+import { InputGroup, FormControl } from 'react-bootstrap';
+
 export default function SearchField() {
+  const [inputCode, setInputCode]: any[] = useState('');
+
+  function hundleInput(event: any) {
+    const { value } = event.target;
+
+    setInputCode(value);
+  }
+
   return (
-    <input type="text" />
+    <InputGroup className="mb-3">
+      <InputGroup.Prepend>
+        <InputGroup.Text>Search</InputGroup.Text>
+      </InputGroup.Prepend>
+      <FormControl
+        name="code"
+        placeholder="Area code"
+        aria-label="Area code"
+        onChange={hundleInput}
+        value={inputCode}
+      />
+    </InputGroup>
   );
 }
